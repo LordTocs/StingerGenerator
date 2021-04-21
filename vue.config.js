e6bc531cb03701fbf428e81693045eaa38849cf0
LordTocs <Tocs1001@gmail.com>
2021-04-20 21:41:35 -0400

    Set public root to /stingers

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
 
 	//Set the output dir to be in /stingers for Netlify proxying.
 	//outputDir: path.resolve(__dirname, "./dist/stingers/"),
+	publicPath: '/stingers/',
 
 	transpileDependencies: [
 		'vuetify'
