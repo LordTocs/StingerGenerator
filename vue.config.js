abc3adb89ce5a9c450296a008decd49c688888c6
LordTocs <lordtocs@gmail.com>
2025-11-12 15:54:30 -0500

    adjust relative path since the domain is dead

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
 	//Set the output dir to be in /stingers for Netlify proxying.
 	//outputDir: path.resolve(__dirname, "./dist/stingers/"),
-	publicPath: '/stingers/',
+	// publicPath: '/stingers/',
 
 	transpileDependencies: [
 		'vuetify'
